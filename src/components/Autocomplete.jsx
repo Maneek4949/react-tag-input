e909685cdbfb672add22aebeb6043a54ce7da833
Manoj Prajapati <69102431+Maneek4949@users.noreply.github.com>
2024-01-17 01:03:00 +0530

    Update

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -85,27 +85,25 @@ const AutoComplete = ({ options = [
   return (
     <div>
 
-      <div className="mt-5 block" ref={autocompleteRef}>
-        <div className="flex">
+      <div className="mt-10 block " ref={autocompleteRef}>
+        <div className="flex border-b-4 border-indigo-500 relative">
         {tags.map((tag, index) => (
           <div 
             key={tag.id}
-            className={`rounded-full flex w-28 gap-2 items-center bg-gray-100 ${lastSelectedIndex === index ? 'selected' : ''}`}
+            className={`px-0.5 mb-2 ml-3 rounded-full w-32 flex justify-around items-center bg-gray-300 ${lastSelectedIndex === index ? 'outline outline-offset-2 outline-blue-500' : ''}`}
           >
-            <img className="w-5 h-5" src={tag.dp}/> <span className="font-medium">{tag.name}</span><span onClick={() => removeTag(tag)}>x</span>
+            <img className="w-5 object-cover" src={tag.dp}/> <span className="font-medium text-sm">{tag.name}</span><span className=" font-medium rounded-none cursor-pointer " onClick={() => removeTag(tag)}>X</span>
           </div>
         ))}
-
-        <input className="w-96 border-b-4 border-indigo-500 px-3 outline-none"
+    <div className="block relative">
+        <input className="ml-3 w-96 px-3 outline-none"
           value={value}
           onChange={handleChange}
-          placeholder="Search"
+          placeholder="Add new user..."
           onFocus={() => setShowSuggestions(true)}
         />
-      </div>
-      </div>
-      {showSuggestions && (
-        <ul className=" w-80 max-h-36 overflow-auto shadow-xl">
+        {showSuggestions && (
+        <ul className="absolute w-80 max-h-36 overflow-auto shadow-xl">
           {options
             .filter(
               (option) =>
@@ -116,15 +114,19 @@ const AutoComplete = ({ options = [
               <li
                 onClick={() => handleSuggestionClick(suggestion)}
                 key={suggestion.id}
-              ><div className="px-1 py-3 flex gap-4 items-center bg-white hover:bg-gray-100">
-                <img className="w-8 h-8" src={suggestion.dp}/> <span className="font-medium">{suggestion.name} </span>
-                <span className="text-gray-800 text-sm">{suggestion.email}</span>
+              ><div className="px-3 py-3 flex justify-between  items-center bg-white hover:bg-gray-200">
+                <div className="flex items-center"> <img className="object-cover w-8 mr-2" src={suggestion.dp}/> <span className="font-medium">{suggestion.name} </span></div>
+               <span className="text-gray-800 text-sm">{suggestion.email}</span>
               </div>
                 
               </li>
             ))}
         </ul>
       )}
+      </div>
+      </div>
+      </div>
+      
     </div>
   );
 };
