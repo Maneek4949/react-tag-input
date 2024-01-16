83b45c4bfdb078ae55319d1ea9f4bcea2951a7a9
Manoj Prajapati <69102431+Maneek4949@users.noreply.github.com>
2024-01-17 01:03:53 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,3 @@ export default function App() {
     </div>
   );
 }
-// options={ [
-//   { id: "1", name: "Oranges", email: "oranges.bing@mail.com", dp: "location" },
-//   { id: "2", name: "Apples", email: "apples.bing@mail.com", dp: "location" },
-//   { id: "3", name: "Pears", email: "pears.bing@mail.com", dp: "location" }
-// ]}
